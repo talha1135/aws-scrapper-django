89b8d153028e81e97c01fdc9cdec796cb16c5f7d
talha1135 <tlhhssain@gmail.com>
2024-12-27 16:47:22 +0500

    first commit

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views import FileUploadView, FileDownloadView
 
 urlpatterns = [
-    path('upload/', FileUploadView.as_view(), name='file-upload'),
-    path('download/<str:filename>/', FileDownloadView.as_view(), name='file-download'),
+ 
 ]
